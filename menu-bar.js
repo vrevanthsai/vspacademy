d0d2f844e43ceb5652ecfbaf8f19d3551cc1d47e
vrevanthsai <135401978+vrevanthsai@users.noreply.github.com>
2023-06-22 20:50:43 +0530

    Add files via upload

---
+++ menu-bar.js
@@ -0,0 +1,6 @@
+mid=document.querySelector('.mid');
+burger=document.querySelector('.buger');
+rmenu=document.querySelector('.rmenu');
+rmenu.addEventListener('click',()=>{
+    mid.classList.toggle('burger');
+})
